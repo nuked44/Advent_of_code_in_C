6befd17e9305e64956f32e04006329f6066fff1e
nuked44 <nuked44@gmail.com>
2023-07-15 07:07:24 +0200

    aoc 2015 10 optimization

--- src/2015/2015_10.c
+++ src/2015/2015_10.c
@@ -2,60 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-void look_and_say(char *input, char *buffer) {
-    int input_len = strlen(input);
-    char *temp_buffer = calloc(input_len + 1, 1);
-    int ammout_char_in_row = 1;
+void look_and_say(const char *input, char **buffer, size_t *buffer_size) {
+    int input_size = strlen(input) + 1;
+    char *input_c = malloc(input_size);
+    strcpy(input_c, input);
+    size_t buffer_pos = 0;
+    int amount_char_in_row = 1;
     char next = '\0';
     char current = '\0';
-    for (size_t i = 0; input[i + 1] != '\0'; i++) {
-        next = input[i + 1];
-        current = input[i];
+
+    for (size_t i = 0; input_c[i + 1] != '\0'; i++) {
+        next = input_c[i + 1];
+        current = input_c[i];
+
         if (next == current) {
-            ammout_char_in_row++;
+            amount_char_in_row++;
         } else {
-            int len = sprintf(temp_buffer, "%d%c", ammout_char_in_row, current);
-            temp_buffer[len + 1] = '\0';
-            strcat(buffer, temp_buffer);
-            ammout_char_in_row = 1;
+            // Resize the buffer if needed
+            if (buffer_pos + 10 >= *buffer_size) {
+                *buffer_size *= 2;
+                *buffer = realloc(*buffer, *buffer_size);
+            }
+            buffer_pos += snprintf(&((*buffer)[buffer_pos]), *buffer_size - buffer_pos, "%d%c", amount_char_in_row, current);
+            amount_char_in_row = 1;
         }
     }
-    sprintf(temp_buffer, "%d%c", ammout_char_in_row, next);
-    strcat(buffer, temp_buffer);
-    free(temp_buffer);
+
+    // Resize the buffer if needed
+    if (buffer_pos + 10 >= *buffer_size) {
+        *buffer_size *= 2;
+        *buffer = realloc(*buffer, *buffer_size);
+    }
+    snprintf(&((*buffer)[buffer_pos]), *buffer_size - buffer_pos, "%d%c", amount_char_in_row, next);
+    free(input_c);
 }
 
-void _2015_10_p1(char *input) {
-    char *input_buffer = malloc(strlen(input) + 1);
-    strcpy(input_buffer, input);
-    char *buffer = calloc(strlen(input) + 1, 2);
+void _2015_10_p1(const char *input) {
+    size_t input_len = strlen(input);
+    
+    size_t buffer_size = input_len * 2 + 1;
+    
+    char *result = malloc(buffer_size);
+    strcpy(result, input);
+
     for (size_t _ = 0; _ < 40; _++) {
-        look_and_say(input_buffer, buffer);
-        input_buffer = realloc(input_buffer, strlen(buffer) + 1);
-        strcpy(input_buffer, buffer);
-        buffer = realloc(buffer, (strlen(buffer) + 1) * 2);
-        buffer[0] = '\0';
+        look_and_say(result, &result, &buffer_size);
     }
-    printf("%zu\n", strlen(input_buffer));
-    free(input_buffer);
-    free(buffer);
+
+    size_t result_len = strlen(result);
+    printf("%zu\n", result_len);
+    free(result);
 }
 
 void _2015_10_p2(char *input) {
-    int input_len = strlen(input);
-    char *input_buffer = malloc(input_len + 1);
-    strcpy(input_buffer, input);
-    char *buffer = calloc(input_len + 1, 2);
+    size_t input_len = strlen(input);
+    
+    size_t buffer_size = input_len * 2 + 1;
+    
+    char *result = malloc(buffer_size);
+    strcpy(result, input);
+
     for (size_t _ = 0; _ < 50; _++) {
-        look_and_say(input_buffer, buffer);
-        input_buffer = realloc(input_buffer, strlen(buffer) + 1);
-        strcpy(input_buffer, buffer);
-        buffer = realloc(buffer, (strlen(buffer) + 1) * 2);
-        buffer[0] = '\0';
+        look_and_say(result, &result, &buffer_size);
     }
-    printf("%zu\n", strlen(input_buffer));
-    free(input_buffer);
-    free(buffer);
+
+    size_t result_len = strlen(result);
+    printf("%zu\n", result_len);
+    free(result);
 }
 
 void run2015_10(char *input) {
